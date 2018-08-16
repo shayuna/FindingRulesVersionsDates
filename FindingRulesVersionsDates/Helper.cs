d7b5698587de88b3d58ed85eabadfa4287406c6f
shayuna <shaiz2206@gmail.com>
2018-08-16 16:17:59 +0300

    version

---
+++ FindingRulesVersionsDates/Helper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using HtmlAgilityPack;
+
+namespace FindingRulesVersionsDates
+{
+    class Helper
+    {
+        public static ICollection<HtmlNode> GetAllHtmlClausesInFileLoadedFromWeb(string sUrl)
+        {
+            return Helper.GetAllHtmlClausesInHtmlDocument(Helper.GetHtmlDocFromUrl(sUrl));
+        }
+        public static ICollection<HtmlNode> GetAllHtmlClausesInHtmlDocument(HtmlDocument oDoc)
+        {
+            
+            ICollection<HtmlNode> arNodesInDoc = oDoc.QuerySelectorAll(".hearot");
+            foreach (HtmlNode oNode in arNodesInDoc)
+            {
+                oNode.SetAttributeValue("class", oNode.GetAttributeValue("class", "") + " " + "comparableItm");
+            }
+            ICollection<HtmlNode> arNodesToReturn = oDoc.QuerySelectorAll(".comparableItm");
+            foreach (HtmlNode oNode in arNodesToReturn)
+            {
+                oNode.SetAttributeValue("class", oNode.GetAttributeValue("class", "").Replace(" comparableItm", ""));
+            }
+            return arNodesToReturn;
+        }
+        public static HtmlDocument GetHtmlDocFromUrl(string sUrl)
+        {
+            HtmlWeb oHtmlWeb = new HtmlWeb();
+            oHtmlWeb.OverrideEncoding = Encoding.GetEncoding(1255);
+            return oHtmlWeb.Load(sUrl);
+        }
+        public static bool WriteToDB(int iC,string sDate)
+        {
+            bool bRslt = true;
+            try
+            {
+                string sDataSrc = "192.168.200.4";
+                string sConnStr = "Initial Catalog=LawData;User ID=sa;Password=;Data Source=" + sDataSrc;
+                SqlConnection connWrite = new SqlConnection(sConnStr);
+                connWrite.Open();
+                SqlCommand cmdWrite = new SqlCommand();
+                cmdWrite.Connection = connWrite;
+                cmdWrite.CommandType = System.Data.CommandType.Text;
+                cmdWrite.CommandText = "update hok_previousversions_legislationdates set dtindoc=convert(datetime,@dt,103) where c=@c";
+                cmdWrite.Parameters.AddWithValue("@c", iC);
+                cmdWrite.Parameters.AddWithValue("@dt", sDate);
+                cmdWrite.ExecuteNonQuery();
+                cmdWrite.Dispose();
+                connWrite.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("oops. something happened when trying to write data to db. versionc=" + iC + " exception is - " + ex.Message);
+                bRslt = false;
+            }
+            return bRslt;
+        }
+    }
+}
--- FindingRulesVersionsDates/Program.cs
+++ FindingRulesVersionsDates/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace FindingRulesVersionsDates
 {
@@ -10,8 +14,49 @@ namespace FindingRulesVersionsDates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world123");
-            Console.ReadKey();
+
+            string sDataSrc = "192.168.200.4";
+            string sConnStr = "Initial Catalog=LawData;User ID=sa;Password=;Data Source=" + sDataSrc;
+            string sSql = "select c,hokc from hok_previousversions_legislationdates where isprocessed=0 order by hokc,c";
+            SqlConnection connRead = new SqlConnection(sConnStr);
+            try
+            {
+                connRead.Open();
+                SqlCommand cmdRead = new SqlCommand(sSql, connRead);
+                SqlDataReader dataReader = cmdRead.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    string sUrl = "https://www.lawdata.co.il/lawdata_face_lift_test/gethok.asp?flnm=" + dataReader.GetValue(1) + "_" + dataReader.GetValue(0) + "&v=1";
+                    ICollection<HtmlNode> arNodes = Helper.GetAllHtmlClausesInFileLoadedFromWeb(sUrl);
+                    DateTime oRuleDt = new DateTime(1900,1,1);
+                    foreach (HtmlNode eNode in arNodes)
+                    {
+                        string sText = eNode.InnerText;
+                        //                        string sPattern ="(ס\"ח|ק\"ת)"+"\\s+\\d+"+"[\\s,]*[א-ת\"]+"+"[\\s,]*[()]"+"([.0-9]+)"+"[()]";
+                        string sPattern = "(ס\"ח|ק\"ת)" + "\\s+\\d+" + "[\\s,]*[א-ת\"]+" + "[\\s,]*[()]" + "([.,\\/0-9]+)" + "[()]";
+                        Regex re = new Regex(sPattern);
+                        MatchCollection arMatches = re.Matches(sText);
+                        foreach (Match mt in arMatches)
+                        {
+                            string sDate = mt.Groups[2].Value;
+                            DateTime oDt= Convert.ToDateTime(sDate, new CultureInfo("he"));
+                            if (oRuleDt == null || DateTime.Compare(oDt, oRuleDt) > 0) oRuleDt = oDt;
+                        }
+                    }
+                    if (DateTime.Compare(oRuleDt, new DateTime(1900, 1, 1)) > 0)
+                    {
+                        Helper.WriteToDB(Convert.ToInt32(dataReader.GetValue(0)), oRuleDt.ToString("dd/MM/yyyy"));
+                    }
+                }
+                dataReader.Close();
+                cmdRead.Dispose();
+                connRead.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("can't open connection to db. error is " + ex.Message);
+            }
         }
     }
 }
